9cc58118323763469677eb56dc53cff9994fda7d
SwagathchandranS <143539202+SwagathchandranS@users.noreply.github.com>
2024-01-15 01:59:46 +0530

    Update Check_leap.py

--- 2/Check_leap.py
+++ 2/Check_leap.py
@@ -1,1 +1,3 @@
-check_leap = lambda year: (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+check_leap = lambda year: (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)  
+# The code checks for the leap year conditions: either divisible by 4 but not by 100, or divisible by 400.
+
